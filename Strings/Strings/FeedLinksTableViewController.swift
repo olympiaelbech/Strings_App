a76b5b68d49a1bc09986aeb624a6a140600b5993
Apple <apple@Apples-MacBook-Pro-10.local>
2019-08-01 12:11:19 -0400

    Incomplete Iteration 2

--- Strings/Strings/FeedLinksTableViewController.swift
+++ Strings/Strings/FeedLinksTableViewController.swift
@@ -1,36 +1,25 @@
 //
-//  MessagesTableViewController.swift
+//  FeedLinksTableViewController.swift
 //  Strings
 //
-//  Created by Apple on 7/31/19.
+//  Created by Apple on 8/1/19.
 //  Copyright © 2019 Olympia Elbech. All rights reserved.
 //
 
 import UIKit
 
-struct Messages {
-    var num : Int
-    var College : String
-    var message : String
+
+struct Feed{
+    var id : Int
+    var name: String
+    var instrument : String
+    var link: String
     
 }
+class FeedLinksTableViewController: UITableViewController {
 
-class MessagesTableViewController: UITableViewController {
-
-    
-    var allColleges = [
-        Messages(num: 1, College:"NYU", message: "Hi we saw your solo on the bach 12 and we are interested in you, please get back to us soon"),
-        Messages(num: 2, College: "Berklee", message: "Hi, your viola solo was really impressive. Do you have any interest in coming to Berklee"),
-        Messages(num: 3 , College: "Bard College", message: "Hello, which version of Vivaldi's Gloria did you play on the violin?"),
-        Messages(num: 4, College: "Manhattan School of Music", message: "Hey Cheyenne, do you live in New York City? Your music is wonderful and we could offer you a scholarship to join our program! Let us know! Sincerely, MSM"),
-        Messages(num: 5 , College: "John Hopkins", message: "Chyenne, you're amazing at music, but we're sure you have other passions, and so do we! Johns Hopkins has amazing programs for music, pre-med, and more. Explore our website at: https://www.jhu.edu/"),
-        Messages(num: 6 , College: "Rice University", message: "Everything is bigger in Texas, and that includes our music program, resources, and campus. I hope you are considering to apply to Rice, our regular decision application deadline is January 1"),
-        Messages(num: 7, College: "UPenn", message: "We saw your video of you playing Beethoven's Symphony No. 5 in C Minor, would you be interested in seeting up an interview?"),
-        Messages(num: 8, College: "Juliard", message: "We viewed your cover of Tchaikovsky's Swan Lake Suite, Op. 20. Your talent intregues us and we would love to host you for an interview."),
-        Messages(num: 9, College: "UChigaco", message: "Hi Cheyenne, we saw the clip of you playing Fauré's Pavane in F-sharp Minor. We would like to talk more.")
-]
-    
-        override func viewDidLoad() {
+    var allFeed = [Feed(id: 1, name: "Cheyenne, Ajebe", instrument: "Bass Trombone", link: "")]
+    override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -39,8 +28,6 @@ class MessagesTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-    
-    // var image : String
 
     // MARK: - Table view data source
 
@@ -51,29 +38,22 @@ class MessagesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return allColleges.count
+        return allFeed.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
         
-        cell.textLabel?.text = allColleges[indexPath.row].College
+        cell.textLabel?.text = allFeed[indexPath.row].name
+        cell.textLabel?.text = allFeed[indexPath.row].instrument
+        cell.textLabel?.text = allFeed[indexPath.row].link
         
-        cell.detailTextLabel?.text = allColleges[indexPath.row].message
         
-        //cell.imageView?.image = allColleges[indexPath.row].num
-    
 
         return cell
     }
     
-    
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
-    {
-        return "Section \(section)"
-    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -118,6 +98,6 @@ class MessagesTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
+ }
+ */
 }
